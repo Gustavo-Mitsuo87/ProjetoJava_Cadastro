9a0abffcec12aadc6219b8f64f9e81d417122513
GuilhermeO-Gomes <guilherme16062009@gmail.com>
2026-05-24 19:11:46 -0300

    Conexão/Util pronto

--- src/util/Conexao.java
+++ src/util/Conexao.java
@@ -1,7 +1,16 @@
 package util;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Conexao {
+	// A primeira parte da URL é padrão do jdbc, a segunda trata-se do endereço do BD, e a terceira parte é o nome do banco
+private static final String URL = "jdbc:mysql://localhost:3306/cadastro_java";
+private static final String USUARIO = "root";
+private static final String SENHA = "";
 
+public static Connection conectar() throws SQLException {
+	return DriverManager.getConnection(URL, USUARIO, SENHA);
+}
 }
